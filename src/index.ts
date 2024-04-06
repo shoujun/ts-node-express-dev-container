634833f13056670ae4c25a28922d1b9d51dde8f6
shoujun_ni <shoujun.ni@nuance.com>
2024-04-06 11:28:29 -0400

    initialize it

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express, { Express, Request, Response } from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app: Express = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server in TypeScript - dev container')
+})
+
+app.post('/example', (req: Request, res: Response) => {
+  // Access POST parameters with req.body
+  console.log(req.body);
+
+  // Send a response
+  res.send('Received your POST request!');
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`)
+})
